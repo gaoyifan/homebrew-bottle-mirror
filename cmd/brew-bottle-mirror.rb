ae6a88b16eee6c259e2b17791084d0e8121a4d1c
Yifan Gao <git@gaoyifan.com>
2016-01-14 16:15:01 +0800

    parallel

--- cmd/brew-bottle-mirror.rb
+++ cmd/brew-bottle-mirror.rb
@@ -1,35 +1,124 @@
 require "formula"
+require 'thread'
 
-Formula.core_files.each do |fi|
-  f = Formula[fi]
-  next unless f.bottle_defined?
-
-  bottle_spec = f.stable.bottle_specification
-  bottle_spec.collector.keys.each do |os|
-    checksum = bottle_spec.collector[os]
-    next unless checksum.hash_type == :sha256
-    filename = Bottle::Filename.create(f, os, bottle_spec.revision)
-    url = "#{bottle_spec.root_url}/#{filename}"
-
-    file = HOMEBREW_CACHE/filename
-    FileUtils.rm_f file
-    next if File.exist?("#{file}.checked")
-
-    begin
-      curl url, "-o", file
-      file.verify_checksum(checksum)
-    rescue ErrorDuringExecution
-      opoo "Failed to download #{url}"
-      FileUtils.rm_f file
-      next
-    rescue ChecksumMismatchError => e
-      opoo e
-      FileUtils.rm_f file
-      next
+class ThreadPool
+  class Worker
+    def initialize
+      @mutex = Mutex.new
+      @thread = Thread.new do
+        while true
+          sleep 0.001
+          block = get_block
+          if block
+            block.call
+            reset_block
+          end
+        end
+      end
     end
-    FileUtils.touch("#{file}.checked")
-    ohai  "#{filename} downloaded"
-
+    
+    def get_block
+      @mutex.synchronize {@block}
+    end
+    
+    def set_block(block)
+      @mutex.synchronize do
+        raise RuntimeError, "Thread already busy." if @block
+        @block = block
+      end
+    end
+    
+    def reset_block
+      @mutex.synchronize {@block = nil}
+    end
+    
+    def busy?
+      @mutex.synchronize {!@block.nil?}
+    end
   end
+  
+  attr_accessor :max_size
+  attr_reader :workers
 
+  def initialize(max_size = 10)
+    @max_size = max_size
+    @workers = []
+    @mutex = Mutex.new
+  end
+  
+  def size
+    @mutex.synchronize {@workers.size}
+  end
+  
+  def busy?
+    @mutex.synchronize {@workers.any? {|w| w.busy?}}
+  end
+  
+  def join
+    sleep 0.01 while busy?
+  end
+  
+  def process(&block)
+    wait_for_worker.set_block(block)
+  end
+  
+  def wait_for_worker
+    while true
+      worker = find_available_worker
+      return worker if worker
+      sleep 0.01
+    end
+  end
+  
+  def find_available_worker
+    @mutex.synchronize {free_worker || create_worker}
+  end
+  
+  def free_worker
+    @workers.each {|w| return w unless w.busy?}; nil
+  end
+  
+  def create_worker
+    return nil if @workers.size >= @max_size
+    worker = Worker.new
+    @workers << worker
+    worker
+  end
+end
+
+
+pool = ThreadPool.new(10)
+Formula.core_files.each do |fi|
+  pool.process do
+    f = Formula[fi]
+    next unless f.bottle_defined?
+
+    bottle_spec = f.stable.bottle_specification
+    bottle_spec.collector.keys.each do |os|
+      checksum = bottle_spec.collector[os]
+      next unless checksum.hash_type == :sha256
+      filename = Bottle::Filename.create(f, os, bottle_spec.revision)
+      url = "#{bottle_spec.root_url}/#{filename}"
+
+      file = HOMEBREW_CACHE/filename
+      FileUtils.rm_f file
+      next if File.exist?("#{file}.checked")
+
+      begin
+        curl url, "-o", file
+        file.verify_checksum(checksum)
+      rescue ErrorDuringExecution
+        opoo "Failed to download #{url}"
+        FileUtils.rm_f file
+        next
+      rescue ChecksumMismatchError => e
+        opoo e
+        FileUtils.rm_f file
+        next
+      end
+      FileUtils.touch("#{file}.checked")
+      ohai  "#{filename} downloaded"
+
+    end
+  end
 end
